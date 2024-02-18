0971c81debe7653be7d75687fecafafe306b279b
Emmanuel Nkubito <emmanuel.nkubito@amalitech.com>
2024-02-18 23:43:44 +0200

    make it more better

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,68 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { MapContainer, GeoJSON } from "react-leaflet";
+import { getCountryCode, getCountryData, getCountryDataList, getEmojiFlag } from 'countries-list'
 import axios from "axios";
 import mapData from "./../data/countries.json";
 import "leaflet/dist/leaflet.css";
 import "./MyMap.css";
 
-
 const MyMap = () => {
   const [color, setColor] = useState("#ffff00");
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [country, setCountry] = useState(null);
 
-  const fetchData = async (country_name) => {
+  const fetchData = async (country_code) => {
     try {
-      const response = await axios.get(`https://csirt-be.onrender.com/teams/countries/${country_name}`);
+      const response = await axios.get(`https://csirt-be.onrender.com/teams/countries/?country=${country_code}`);
       setData(response.data);
+      console.log("------response--------", response.data)
     } catch (error) {
       setError(error);
     }
   };
 
-
   useEffect(() => {
-    console.log(mapData);
-  }, []);
-
-  const countryStyle = {
-    fillColor: "red",
-    fillOpacity: 1,
-    color: "black",
-    weight: 2,
-  };
+    if (country) {
+      fetchData(getCountryCode(country));
+    }
+    console.log("---------------data----------::", data);
+  }, [country]);
 
   const africaCenter = [4.6, 10.5]; // Center coordinates for Africa
 
-  const changeCountryColor = (event) => {
-    console.log("----------------",event.target.feature.properties.ADMIN);
-    fetchData(event.target.feature.properties.ADMIN)
-    // event.target.setStyle({
-    //   color: "green",
-    //   fillColor: color,
-    //   fillOpacity: 1,
-    // });
+  const changeCountryCode = (event) => {
+    const countryName = event.target.feature.properties.ADMIN;
+    setCountry(countryName);
   };
-  
 
   const onEachCountry = (country, layer) => {
     const countryName = country.properties.ADMIN;
-    console.log(countryName);
-   
-    layer.bindPopup(countryName);
-    // passing country name to function
 
-    layer.options.fillOpacity = Math.random(); //0-1 (0.1, 0.2, 0.3)
-    // const colorIndex = Math.floor(Math.random() * colors.length);
-    // layer.options.fillColor = colors[colorIndex]; //0
-
+    layer.bindPopup(getPopupContent(countryName));
+    
     layer.on({
-      click: changeCountryColor,
+      click: changeCountryCode,
     });
   };
 
-  const colorChange = (event) => {
-    setColor(event.target.value);
+  const getPopupContent = (countryName) => {
+    if (data) {
+      let popupContent = `<b>${countryName}</b><br/><ul>`;
+      
+      data.forEach(team => {
+          popupContent += `<li>Team: ${team.name}</li>`;
+      });
+
+      popupContent += "</ul>";
+      
+      return popupContent;
+    } else {
+      return `<b>${countryName}</b><br/>Loading data...`;
+    }
   };
 
   return (
@@ -70,12 +67,11 @@ const MyMap = () => {
       <h1 style={{ textAlign: "center" }}>My Map</h1>
       <MapContainer style={{ height: "85vh" }} zoom={4} center={africaCenter}>
         <GeoJSON
-          style={countryStyle}
+          style={{ fillColor: "red", fillOpacity: 1, color: "black", weight: 2 }}
           data={mapData.features}
           onEachFeature={onEachCountry}
         />
       </MapContainer>
-      {/* <input type="color" value={color} onChange={colorChange} /> */}
     </div>
   );
 };
